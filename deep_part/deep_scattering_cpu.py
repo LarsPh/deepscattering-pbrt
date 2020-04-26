4bf96a38641f95033b0be9d12279c20a4de765ee
Zhaorong Wang <larsphalin@gmail.com>
2020-04-26 23:24:36 +0800

    Testing

--- deep_part/deep_scattering_cpu.py
+++ deep_part/deep_scattering_cpu.py
@@ -188,7 +188,7 @@ class BulkGenerator():
         else:
             self.accessTimes = fileRecordsNum // recordsNum + 1
         self.mapSize = mapSize
-        assert(self.accessTimes == 5)
+        assert(self.accessTimes == 1)
         self.dsLMDB = None
         valiFileSize = self.fileNum / maxEpoch
         self.maxEpoch = maxEpoch
@@ -372,7 +372,7 @@ class Train():
                         break
 
                 # for testing
-                if (datasetCount == 4):
+                if (datasetCount == 0):
                     break
 
                 datasetCount += 1
@@ -444,8 +444,8 @@ if __name__ == '__main__':
         "dataPath": "/home/LarsMPace/ds_db/",
         "modelPath": "/home/LarsMPace/sync/models/",
         "folds": 1,  # 6, folds number for cross validation, each fold contain at least one image
-        "fileRecordsNum": 2400 * 1200,  # samples for file 1196, 18GB
-        "recordsNum": 2400 * 1200 // 5 + 1,  # data size load in memory, 0.9GB
+        "fileRecordsNum": 1589429,  # samples for file 1196, 18GB
+        "recordsNum": 1589429 # // 5 + 1,  # data size load in memory, 0.9GB
         "trainBatchSize": 500,
         "valiBatchSize": 8000,
         "mapSize": 1048576 * 1024 * 36,  # 1GB * 4096 = 36GB
